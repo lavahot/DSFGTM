a3c9ef43825afd7b1211933381523d3aad1fe150
Taylor Mansfield <lavahot@gmail.com>
2012-10-28 22:30:16 -0700

    Finished with stat module for the night. One function left to implement. Will continue work tomorrow.

--- project4/project4/StatModule.h
+++ project4/project4/StatModule.h
@@ -3,6 +3,7 @@
 
 #include "job.h"
 #include <fstream>
+#include <iomanip>
 
 class StatModule
 {
@@ -34,7 +35,7 @@ class StatModule
 		void reportJobBegin(Job *jobbegin);
 		void reportCPUIdle(float, int);
 		void printStats();
-        void printStatus();
+        void printStatus(int);
 };
 
 StatModule::StatModule()
@@ -57,6 +58,21 @@ StatModule::StatModule()
 }
 StatModule::StatModule(std::ofstream &fileo)
 {//use this constructor
+    //initialize cpu idle array
+    for(int i =0; i<10; i++)
+    {
+        CPUIdle[i]=0;
+    }
+    //initialize stat array
+    for(int i=0;i<2;i++)
+    {
+        for(int j = 0; j<4; j++)
+        {
+            jobStatArry[i][j].numOfType=0;
+            jobStatArry[i][j].totCPUQ=0;
+            jobStatArry[i][j].totWaitQ=0;
+        }
+    }
     fileOut=&fileo;
 }
 StatModule::~StatModule()
@@ -100,8 +116,97 @@ void StatModule::printStats()
     
 }
 
-void StatModule::printStatus()
+void StatModule::printStatus(int ctime)
 {//use to print periodic stats
+    *fileOut<<"Current status"<<std::endl;
+    *fileOut<<"Number of jobs:"<<std::endl;
+    *fileOut<<std::right<<std::setw(20)<<"I/O Type:"<<std::setw(15)<<"CPU Type:"<<std::endl;
+    for(int i =0; i<4; i++)
+    {
+        int rowtemp=0;
+        switch (i) {
+            case 0:
+                rowtemp=10;
+                break;
+            case 1:
+                rowtemp=20;
+                break;
+            case 2:
+                rowtemp=30;
+                break;
+            case 3:
+                rowtemp=60;
+                break;
+            default:
+                break;
+        }
+        *fileOut<<std::setw(10)<<std::left<<i<<" sec"<<std::right;
+        for(int j =0; j<2; j++)
+        {
+            *fileOut<<std::setw(10+5*j)<<jobStatArry[j][i].numOfType;
+        }
+        *fileOut<<std::endl;
+        
+    }
+    *fileOut<<std::endl<<"Average wait queue time of completed jobs:"<<std::endl;
+    *fileOut<<std::right<<std::setw(20)<<"I/O Type:"<<std::setw(15)<<"CPU Type:"<<std::endl;
+    for(int i =0; i<4; i++)
+    {
+        int rowtemp=0;
+        switch (i) {
+            case 0:
+                rowtemp=10;
+                break;
+            case 1:
+                rowtemp=20;
+                break;
+            case 2:
+                rowtemp=30;
+                break;
+            case 3:
+                rowtemp=60;
+                break;
+            default:
+                break;
+        }
+        *fileOut<<std::setw(10)<<std::left<<i<<" sec"<<std::right;
+        for(int j =0; j<2; j++)
+        {
+            *fileOut<<std::setw(10+5*j)<<jobStatArry[j][i].totWaitQ/jobStatArry[j][i].numOfType;
+        }
+        *fileOut<<std::endl;
+    }
+    *fileOut<<std::endl<<"Average CPU queue time of jobs:"<<std::endl;
+    *fileOut<<std::right<<std::setw(20)<<"I/O Type:"<<std::setw(15)<<"CPU Type:"<<std::endl;
+    for(int i =0; i<4; i++)
+    {
+        int rowtemp=0;
+        switch (i) {
+            case 0:
+                rowtemp=10;
+                break;
+            case 1:
+                rowtemp=20;
+                break;
+            case 2:
+                rowtemp=30;
+                break;
+            case 3:
+                rowtemp=60;
+                break;
+            default:
+                break;
+        }
+        *fileOut<<std::setw(10)<<std::left<<i<<" sec"<<std::right;
+        for(int j =0; j<2; j++)
+        {
+            *fileOut<<std::setw(10+5*j)<<jobStatArry[j][i].numOfType;
+        }
+        *fileOut<<std::endl;
+        
+    }
+    *fileOut<<std::endl<<std::endl;
+    
     
 }
 
