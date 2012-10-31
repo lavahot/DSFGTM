5caae47e9d24bfce417791088b0c338ebd50897c
Frank J Greenhalgh <greenhal@W-72.ecc.unr.edu>
2012-10-30 22:20:54 -0700

    Added main.cpp

--- project4/project4/main.cpp
+++ project4/project4/main.cpp
@@ -15,14 +15,40 @@ using namespace std;
 
 int main()
 {
- StatModule *stat_mod = new StatModule();
+ ofstream fout( "StatOutput.txt" );
+
+ StatModule *stat_mod = new StatModule(fout);
+ double p_job_per_cycle[3];
+ double p_serv_time_dist[4];
+ double p_job_id[2];
+ int num_jobs;
+
+ cout << endl << "Enter the jobs per cycle distribution" << endl;
+ cout << "Probability of 0 jobs: "; cin>> p_job_per_cycle[0];
+ cout << "Probability of 1 jobs: "; cin>> p_job_per_cycle[1];
+ cout << "Probability of 2 jobs: "; cin>> p_job_per_cycle[2];
+
+ cout << endl << "Enter the service time distribution" << endl;
+ cout << "Probability of service time 10.0: "; cin>> p_serv_time_dist[0];
+ cout << "Probability of service time 15.0: "; cin>> p_serv_time_dist[1];
+ cout << "Probability of service time 20.0: "; cin>> p_serv_time_dist[2];
+ cout << "Probability of service time 30.0: "; cin>> p_serv_time_dist[3];
+
+ cout << endl << "Enter the id distribution" << endl;
+ cout << "Probability of Proc jobs: "; cin>> p_job_id[0];
+ cout << "Probability of IO jobs: "; cin>> p_job_id[1];
+
+ cout << endl << "Enter the total number of jobs" << endl;
+ cout << "Number of jobs: "; cin >> num_jobs;
+
  SimEngine sim_eng(stat_mod, 
 	p_jobs_per_cycle_distribution,
 	double p_service_time_distribution,
-	double p_job_id_distribution ,
-	double total_time);
+	p_job_id_distribution ,
+	num_jobs);
  sim_eng.Sim();
  stat_mod.printstats();
     
+ fout.close();
 }
 
